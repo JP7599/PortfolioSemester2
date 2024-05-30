4e2d4901a3496a5d338dfff7189d1e5063873300
JP7599 <yampi123@gmail.com>
2024-05-30 05:50:20 +0200

    scroll animation

--- static/js/scriptHome.js
+++ static/js/scriptHome.js
@@ -11,24 +11,23 @@ window.addEventListener("load", () => {
         end: "+=150%",
         pin: true,
         scrub: true,
-        markers: true
+        markers: true,
+        onUpdate: self => {
+          if (self.progress === 1) {
+            document.querySelector("#home").classList.add("show-section");
+            document.querySelector(".image-container").style.display = "none";
+            document.querySelector(".section.hero").style.display = "none";
+          }
+        }
       }
     })
-    .to("img", {
+    .to(".image-container img", {
       scale: 2,
-      z: 350,
+      y: 200, // Adjust this value to zoom in a bit higher than the head
       transformOrigin: "center center",
       ease: "power1.inOut"
-    })
-    .to(
-      ".section.hero",
-      {
-        scale: 1.1,
-        transformOrigin: "center center",
-        ease: "power1.inOut"
-      },
-      "<"
-    );
+    });
 });
+
 
 
